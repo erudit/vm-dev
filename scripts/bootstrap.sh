6741a9dcacb3d5223b060f672a2ec257e68f014d
Alexandre Guédon <alexandre.guedon@erudit.org>
2015-07-17 15:23:49 -0400

    Move X11 to Packer, fix VGuestAdditions resizing

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,9 +6,10 @@ if [ -f "$SHARED_DIR/scripts/config" ]; then
   . $SHARED_DIR/scripts/config
 fi
 
-sudo yum update
-sudo yum -y install git 
-sudo yum -y groupinstall "GNOME Desktop" "Graphical Administration Tools"
-sudo ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target
+#sudo yum update
+#sudo yum -y install git 
+#
+#sudo yum -y groupinstall "GNOME Desktop" "Graphical Administration Tools"
+#sudo ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target
 
 sudo systemctl stop firewalld
