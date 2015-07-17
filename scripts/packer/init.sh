823f2cfceb0c3e1e6ecc4024855ecd70ffabc957
Alexandre Guédon <alexandre.guedon@erudit.org>
2015-07-17 16:58:56 -0400

    fixer bg gnome (todo)

--- scripts/packer/init.sh
+++ scripts/packer/init.sh
@@ -7,3 +7,6 @@ yum -y install gcc kernel-devel kernel-headers perl cpp dkms make bzip2 git
 # Installing X11 / Gnome / Configuration tools
 sudo yum -y groupinstall "GNOME Desktop" "Graphical Administration Tools"
 sudo ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target
+
+# cp /vagrant/erudit.png /home/vagrant/Pictures/erudit.png # Dir exists?"
+# gsettings set org.gnome.desktop.background picture-uri file:///home/vagrant/Pictures/erudit.png
