c416f2ada47f9ec83cc0cbc592b73392a74b36a6
philippe <tchevass@gmail.com>
2015-07-24 13:27:50 -0400

    Added tomcat configuration scripts

--- scripts/ansible/init.sh
+++ scripts/ansible/init.sh
@@ -2,5 +2,7 @@
 
 SHARED_DIR=$1
 
+export ANSIBLE_CONFIG=$SHARED_DIR/ansible/ansible.cfg
+echo $ANSIBLE_CONFIG
 #Ansible commands
-ansible-playbook -i $SHARED_DIR/ansible/hosts $SHARED_DIR/ansible/vm.yml --connection=local --sudo 
+ansible-playbook $SHARED_DIR/ansible/vm.yml --connection=local --sudo 
