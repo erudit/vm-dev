941b95f9a388fbec0c738c1254a75c7100182bb9
Philippe Dagenais <tchevass@gmail.com>
2016-01-12 14:12:02 -0500

    Init.sh now add epel repo + install ansible

--- scripts/ansible/init.sh
+++ scripts/ansible/init.sh
@@ -2,7 +2,10 @@
 
 SHARED_DIR=$1
 
+sudo rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+sudo yum -y install ansible
+
 export ANSIBLE_CONFIG=$SHARED_DIR/ansible/ansible.cfg
 echo $ANSIBLE_CONFIG
 #Ansible commands
-ansible-playbook $SHARED_DIR/ansible/vm.yml --connection=local  
+ansible-playbook $SHARED_DIR/ansible/vm.yml --connection=local
