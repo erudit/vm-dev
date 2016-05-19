3319b9e742085faa0a39e8be93c31ace95ba90f4
Philippe Dagenais <tchevass@gmail.com>
2016-05-19 15:17:43 -0400

    Fixed repository link

--- scripts/ansible/init.sh
+++ scripts/ansible/init.sh
@@ -2,7 +2,7 @@
 
 SHARED_DIR=$1
 
-sudo rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+sudo yum -y install epel-release
 sudo yum -y install ansible
 
 export ANSIBLE_CONFIG=$SHARED_DIR/ansible/ansible.cfg
