30d2e83d5e448bf99ba5c9b01f81696d12d76d38
Philippe Dagenais <tchevass@gmail.com>
2016-05-20 13:46:16 -0400

    Fixed ansible provision script

--- scripts/ansible/init.sh
+++ scripts/ansible/init.sh
@@ -7,5 +7,6 @@ sudo yum -y install ansible
 
 export ANSIBLE_CONFIG=$SHARED_DIR/ansible/ansible.cfg
 echo $ANSIBLE_CONFIG
+cd $SHARED_DIR/ansible
 #Ansible commands
 ansible-playbook $SHARED_DIR/ansible/vm.yml --connection=local
